18eb3798251d29bb712aa0bdb9e2bfdb2f71b6f0
Bruno Apostolico <bapostolico@gmail.com>
2023-06-17 16:09:24 -0300

    Criação das Classes de Modelo

---
+++ src/main/java/br/com/infnet/model/Assinatura.java
@@ -0,0 +1,44 @@
+package br.com.infnet.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+    public class Assinatura {
+        private BigDecimal mensalidade;
+        private LocalDate begin;
+        private Optional<LocalDate> end;
+        private Cliente cliente;
+
+        public Assinatura(BigDecimal mensalidade, LocalDate begin, Cliente cliente) {
+            this.mensalidade = mensalidade;
+            this.begin = begin;
+            this.end = Optional.empty();
+            this.cliente = cliente;
+        }
+
+        public Assinatura(BigDecimal mensalidade, LocalDate begin, LocalDate end, Cliente cliente) {
+            this.mensalidade = mensalidade;
+            this.begin = begin;
+            this.end = Optional.of(end);
+            this.cliente = cliente;
+        }
+
+        public BigDecimal getMensalidade() {
+            return mensalidade;
+        }
+
+        public LocalDate getBegin() {
+            return begin;
+        }
+
+        public Optional<LocalDate> getEnd() {
+            return end;
+        }
+
+        public Cliente getCliente() {
+            return cliente;
+        }
+}
+
+
---
+++ src/main/java/br/com/infnet/model/Produto.java
@@ -0,0 +1,28 @@
+package br.com.infnet.model;
+
+import java.math.BigDecimal;
+import java.nio.file.Path;
+
+public class Produto {
+    private String nome;
+    private Path file;
+    private BigDecimal preco;
+
+    public Produto(String nome, Path file, BigDecimal preco) {
+        this.nome = nome;
+        this.file = file;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Path getFile() {
+        return file;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+}
